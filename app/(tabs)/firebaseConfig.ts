bceebc297b79a1f4b3eed368530f814a988d31c8
Himmet <137326333+Carbon20@users.noreply.github.com>
2026-02-17 07:27:38 +0300

    Update firebaseConfig.ts

--- app/(tabs)/firebaseConfig.ts
+++ app/(tabs)/firebaseConfig.ts
@@ -6,14 +6,7 @@ import { Database, getDatabase } from "firebase/database";
 import { Firestore, getFirestore } from "firebase/firestore";
 
 // --- SENİN BİLGİLERİN (KONSOLDAN ALDIKLARIN) ---
-const firebaseConfig = {
-  apiKey: "AIzaSyBk2oQq0E59GQLo9Jg_z-TpmftHES-u5yM",
-  authDomain: "petfeeder-todo.firebaseapp.com",
-  projectId: "petfeeder-todo",
-  storageBucket: "petfeeder-todo.firebasestorage.app",
-  messagingSenderId: "834238545050",
-  appId: "1:834238545050:web:1bfa82c0819e1cf70d7323"
-};
+
 
 // Firebase'i başlat
 const app: FirebaseApp = initializeApp(firebaseConfig);
